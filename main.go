cd27399850999ca37ea8d141b693e40381eae5e6
a6u6akera6 <a6u6aker@gmail.com>
2026-03-07 12:38:16 +0300

    GO Servers: chapter 2 completed

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+// API Status
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 
 	port := "8080"
 	filepathRoot := "."
+	cfg := &apiConfig{}
 
 	// Create a new ServeMux
 	serverMux := http.NewServeMux()
@@ -20,16 +28,27 @@ func main() {
 	}
 
 	// Add handler
-	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	serverMux.HandleFunc("/healthz", handlerReadiness)
+	serverMux.Handle("/app/", cfg.middlewareMetricInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	serverMux.HandleFunc("GET /api/healthz", handlerReadiness)
+	serverMux.HandleFunc("GET /admin/metrics", cfg.handlerMetrics)
+	serverMux.HandleFunc("POST /api/reset", cfg.handlerReset)
 
 	// Start the server
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 }
 
-func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+func (cfg *apiConfig) middlewareMetricInc(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>",
+		cfg.fileserverHits.Load())))
 }
